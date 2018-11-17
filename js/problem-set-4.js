850de70eebaa8d80a81bcf7166a6d0f31253c56b
John Turner <jturner@ucvts.org>
2018-11-16 20:49:39 -0500

    fixed button 10

--- js/problem-set-4.js
+++ js/problem-set-4.js
@@ -236,16 +236,16 @@ function change() {
   let penny = Math.floor(pennies-(quarters*25)-(dimes*10)-(nickels*5));
   let coins = quarters + dimes + nickels + penny;
 
-  if coins = 1 {
-  var div = document.getElementById("output10");
-  div.innerHTML= coins + " coin.";
+  if (coins = 1) {
+    var div = document.getElementById("output10");
+    div.innerHTML= coins + " coin.";
 }
 
-  if coins > 1 {
+  if (coins > 1) {
     var div = document.getElementById("output10");
     div.innerHTML= coins + " coins.";
   }
-  
+
 
 
   ////////////////////////// DO NOT MODIFY
